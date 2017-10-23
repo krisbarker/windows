104b61e7cc3cf145e93dc3e6a779543a4de01aae
Kris Barker <kris@thisworldisbig.co.uk>
2017-10-23 20:23:21 +0100

    First PyQt5 fie

--- firsttest.py
+++ firsttest.py
@@ -4,3 +4,21 @@
 # A simple windows interface for running the
 # volume checker for the oil tank
 
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(450, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
